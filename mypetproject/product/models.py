59bf8572d8c52ca4e14dc0e24d6277f4ca3cc5b3
VFarsiyants <farsiyanc@icloud.com>
2022-06-02 07:44:40 +0300

    Lesson-5 practical task

--- mypetproject/product/models.py
+++ mypetproject/product/models.py
@@ -1,4 +1,3 @@
-from operator import mod
 from django.db import models
 from django.utils.translation import gettext as _
 
@@ -9,6 +8,15 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=30, decimal_places=2 ,verbose_name=_('Цена товара'))
     measure_unit = models.CharField(max_length=10, verbose_name=_('Единица измерения'))
     supplier_name = models.CharField(max_length=100, verbose_name=_('Имя поставщика'))
+    category = models.ForeignKey('product.productcategory', on_delete=models.CASCADE,
+                                 verbose_name=_('Категория товара'))
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100, verbose_name=_('Название категории'))
 
     def __str__(self):
         return f'{self.name}'
--- mypetproject/product/urls.py
+++ mypetproject/product/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import ProductListView
+from .views import ProductCategoryView
+
+app_name = 'product'
 
 urlpatterns = [
-    path('', ProductListView.as_view())
+    path('', ProductListView.as_view(), name='product'),
+    path('category/<int:category_id>', ProductCategoryView.as_view(), name='category')
 ]
